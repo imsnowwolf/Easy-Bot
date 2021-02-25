c5ff7a461a346e9936db3f2bb21d3e0c022ac57b
snow_wolf <70009975+imsnowwolf@users.noreply.github.com>
2021-02-25 18:09:16 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { Client, Collection } = require('discord.js'); 
+const { TOKEN, PREFIX } = require('./config');
+
+const client = new Client(); 
+client.commands = new Collection();
+
+const loadCommands = (dir = "./commands/") => {
+    fs.readdirSync(dir).forEach(dirs => {
+    const commands = fs.readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith('.js'));
+
+        for (const file of commands) {
+            const fileName = require(`${dir}/${dirs}/${file}`);
+            client.commands.set(fileName.help.name, fileName);
+        }
+    })
+    console.log(`✅ ${client.commands.size} commandes chargées avec succés !`);
+}
+
+loadCommands()
+
+client.on("ready", () => { 
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("!help", { type: 3});
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+   
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName)
+    if(!command) return;
+   
+    client.commands.get(commandName).execute(client, message, args);
+   });
+
+client.login(TOKEN);
